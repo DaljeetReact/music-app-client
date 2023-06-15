a69f85c862fce04653cd32c4e3f7655b9db7bbba
Daljeet singh <daljeetsingh9911@gmail.com>
2023-06-15 17:36:09 +0200

    add and configure the persist storage for redux

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import thunk from 'redux-thunk';
+
 
 import userDataReducer from './reducers'
 
-export const store = configureStore({ reducer: userDataReducer })
+
+const persistConfig = {
+    key: 'root',
+    storage,
+  };
+const persistedReducer = persistReducer(persistConfig, userDataReducer);
+
+export const store = configureStore({ reducer: persistedReducer,  middleware: [thunk]
+});
+export const persistor = persistStore(store);
 // The store now has redux-thunk added and the Redux DevTools Extension is turned on
