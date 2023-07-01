d6b7166ca42b9051a8ec54c8f272ffd35931ee1f
Daljeet singh <daljeetsingh9911@gmail.com>
2023-07-01 10:20:17 +0200

    remove extra code form song components

--- src/pages/admin/Songs.jsx
+++ src/pages/admin/Songs.jsx
@@ -1,33 +1,12 @@
 import {useState} from 'react';
 import { useSelector } from 'react-redux';
-import { motion } from "framer-motion";
 import { NavLink } from 'react-router-dom';
 
-import { DeleteFileFromFireStore } from './components';
 import CardInfo from './components/CardInfo'
 function Songs() {
 
   const songs =  useSelector(state=>state.songs);
   const [search, setSearch] = useState("");
-
-
-  const SongCard = ({song,key}) =>{
-   const {name,imgUrl,songUrl,album,artist,category,_id} = song;
-    return(
-      <motion.div className='overflow-hidden border-2 rounded-lg shadow-lg max-w-[210px] max-h-[210px] flex justify-center items-center flex-col cursor-pointer relative' key={_id}>
-         <motion.img 
-          whileHover={{scale:1.05}}
-          src={imgUrl}
-          className='h-[150px] w-full object-contain'
-        />
-        <div className='text-center leading-9 py-4'>
-          <p>{name}</p>
-          <p>{artist}</p>
-        </div>
-        <DeleteFileFromFireStore url={imgUrl} type={"albums"} id={_id} songUrl={songUrl} />
-      </motion.div>
-    )
-  }
 
   return (
     <div className='w-[80%] m-auto border-2 mt-20 p-5 shadow-sm rounded-lg relative  backdrop-blur-xl'>
