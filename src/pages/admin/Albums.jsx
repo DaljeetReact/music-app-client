7227f86b056326871f273daad9aa16f2851bb765
Daljeet singh <daljeetsingh9911@gmail.com>
2023-07-03 17:38:20 +0200

    add new logic for total song for album

--- src/pages/admin/Albums.jsx
+++ src/pages/admin/Albums.jsx
@@ -1,16 +1,38 @@
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 
 import CardInfo from "./components/CardInfo"
+import axios from 'axios';
+
+import {songsApi} from '../../utils/index'
 
 
 function Albums() {
 
   const albumsInfo = useSelector(state => state.albums);
   const [search, setSearch] = useState("");
-
-
+  const [fullAlbumDetails, setFullAlbumDetails] = useState([]);
+  const [isLoading, setisLoading] = useState(false);
+   
+  const TotalSongs = async (album)=>{
+    await axios.get(`${songsApi}/album/${album.name}`).then(({data})=>{
+      let albumApped = {...album,songsList:data.data};
+      setFullAlbumDetails(prev=>([ ...prev, albumApped]));
+    }).catch(e=>console.log(e));
+  }
+  useEffect(() => {
+    if(albumsInfo){
+      setisLoading(true);
+      // set fullAlbumDetails to empty before new push
+      setFullAlbumDetails([]);
+      albumsInfo.forEach((album,index) => {
+        TotalSongs(album)
+      });
+      setisLoading(false);
+    }
+  }, []);
+  
   return (
     <div className='w-[80%] m-auto border-1 border-black mt-20 p-5 shadow-sm rounded-xl relative  backdrop-blur-3xl overflow-hidden '>
       <div className="absolute top-0 bottom-0 left-0 right-0  bg-white opacity-[0.5]"></div>
@@ -29,11 +51,14 @@ function Albums() {
       </div>
 
       <div className='grid grid-cols-4 gap-4 justify-center items-center '>
-        {albumsInfo.length > 0 ? (
+        {isLoading?(
+          <div>Loading...</div>
+        ):(fullAlbumDetails.length > 0 )?(
           <>
-            {albumsInfo?.map((album, index) => (
+            {fullAlbumDetails?.map((album, index) => (
               <CardInfo imgUrl={album.imgUrl} key={album._id} type={"album"}>
                 <h3>{album.name}</h3>
+                <div >Total songs:{album?.songsList?.length}</div>
               </CardInfo>
             ))}
           </>
