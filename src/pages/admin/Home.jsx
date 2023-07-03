6df4232e8aebe62f6a9807a219b0506205ebfdf9
Daljeet singh <daljeetsingh9911@gmail.com>
2023-07-03 15:12:26 +0200

    add link to pages in dashboard card

--- src/pages/admin/Home.jsx
+++ src/pages/admin/Home.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { NavLink } from 'react-router-dom'
 
-const InfoCard = ({name,count})=>{
+const InfoCard = ({name,count,link})=>{
   return(
-    <div className='flex flex-col w-40 h-40 p-4 justify-center align-middle bottom-1 rounded-lg shadow-lg cursor-pointer bg-white hover:scale-110 transition-all ease-in-out text-center'>
-      <p className='text-sm font-semibold'>{name}</p>
-      <p className='text-xs'>{count}</p>
-    </div>
+    <NavLink to={`/dashboard/${link}`}>
+      <div className='flex flex-col w-40 h-40 p-4 justify-center align-middle bottom-1 rounded-lg shadow-lg cursor-pointer bg-white hover:scale-110 transition-all ease-in-out text-center'>
+        <p className='text-sm font-semibold'>{name}</p>
+        <p className='text-xs'>{count}</p>
+      </div>
+    </NavLink>
   )
 }
 
@@ -15,10 +18,10 @@ function Home() {
 
   return (
     <div className='w-full flex align-middle p-4 justify-evenly mt-20 '>
-      <InfoCard name={"Users"} count={users?.length}/>
-      <InfoCard name={"Songs"}  count={songs?.length}/>
-      <InfoCard name={"Artists"} count={artists?.length}/>
-      <InfoCard name={"Albums"} count={albums?.length}/>
+      <InfoCard name={"Users"} count={users?.length} link={"users"}/>
+      <InfoCard name={"Songs"}  count={songs?.length} link={"songs"}/>
+      <InfoCard name={"Artists"} count={artists?.length} link={"artists"}/>
+      <InfoCard name={"Albums"} count={albums?.length} link={"albums"}/>
     </div>
   )
 }
